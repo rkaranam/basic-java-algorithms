e9f40cc242dbf070cb36199ecfd14faddac61dd7
rkaranam <karanam.rajasekhar44@gmail.com>
2016-06-27 19:20:54 +0530

    JukeBox 1.0: Basic Implementation

---
+++ src/com/headfirstjava/datastructures/JukeBoxV1.java
@@ -0,0 +1,56 @@
+package com.headfirstjava.datastructures;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class JukeBoxV1 {
+
+	// store song titles in array list of strings
+	ArrayList<String> songsList = new ArrayList<String>();
+	
+	public static void main(String[] args) {
+		JukeBoxV1 jukebox = new JukeBoxV1();
+		jukebox.go();
+	}
+	
+	// load the file and print the contents in songList ArrayList
+	public void go() {
+		getSongs();
+		System.out.println(songsList);
+	}
+	
+	// read file and call addSong method for each line
+	public void getSongs() {
+		
+		File file;
+		BufferedReader fileReader;
+		
+		try {
+			file = new File(System.getProperty("user.dir") + "/SongList.txt");
+			fileReader = new BufferedReader(new FileReader(file));
+			
+			String line = null;
+			while (null != (line = fileReader.readLine())) {
+				addSong(line);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	// break line into two tokens and store song name in array list
+	public void addSong(String lineToParse) {
+		System.out.println("Line : " + lineToParse);
+		String[] tokens = lineToParse.split("/");
+		for (String token : tokens) {
+			System.out.println("# " + token);
+		}
+		System.out.println();
+		songsList.add(tokens[0]);
+	}
+
+}
